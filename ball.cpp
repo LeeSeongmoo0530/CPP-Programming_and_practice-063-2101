c59d042f24bad0aa38132daed8dbdd1e8acf2164
LeeSeongmoo0530 <84953185+LeeSeongmoo0530@users.noreply.github.com>
2021-06-07 22:39:14 +0900

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -24,15 +24,16 @@ int Ball::f_Ball_Get_Direct() const
 {
 	return Ball_Direct;
 }
-int Ball :: f_Ball_Get_X() const
+int Ball::f_Ball_Get_X() const
 {
 	return BallX;
 }
-int Ball::f_Ball_Get_Y() const 
+int Ball::f_Ball_Get_Y() const
 {
 	return BallY;
 }
 
+//공의 생성자
 Ball::Ball()
 {
 	BallX = 24;
@@ -40,8 +41,8 @@ Ball::Ball()
 }
 
 //2.초기발사 왼쪽위 or오른쪽위
-//처음에 InitBall호출
-void Ball:: f_InitBALL()
+//처음에 Main에서 SpaceBar를 눌러 InitBall호출
+void Ball::f_InitBALL()
 {
 	BallX = 24;
 	BallY = 29;
@@ -49,14 +50,14 @@ void Ball:: f_InitBALL()
 
 	srand((unsigned int)time(NULL));
 
-	int Startdirect= rand() % 2;
+	int Startdirect = rand() % 2;
 	if (Startdirect == 1)
 	{
 		Ball_Direct = Right_top;
 		BallMoveX = true;
 		BallMoveY = true;
 	}
-	
+
 	//왼쪽위
 	else
 	{
@@ -71,35 +72,38 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 {
 	if (!ballobj->InitBallMove)
 		return false;
-
+	//공의 상하좌우 좌표
 	int BallTop = ballobj->BallY;
 	int BallBottom = ballobj->BallY + R;
 	int BallLeft = ballobj->BallX;
 	int BallRight = ballobj->BallX + 2 * R;
 
+	//벽돌의 상하좌우 좌표
 	int BrickNumY = 3, BrickNumX = 20;
 	int BrickX, BrickY;
 	int BrickLeft, BrickRight, BrickTop, BrickBottom;
+
 	// Brick[]-XY블록 2차원 배열 ,  BrickNumX Y - X, Y 블록갯수 
 	for (int y = 0; y < BrickNumY; y++)
 	{
-		for (int x = 0; x< BrickNumX; x++)
+		for (int x = 0; x < BrickNumX; x++)
 		{
 
 			// 벽돌이 죽어있는 경우 다음 블록으로
 			//블록이 살아있는지
-			if (!Brickobj->get_display_signal(x,y))
+			if (!Brickobj->get_display_signal(x, y))
 			{
 				continue;
 			}
 
-			BrickLeft = Brickobj->get_x(x,y);
+			BrickLeft = Brickobj->get_x(x, y);
 			BrickRight = BrickLeft + BrickWidth;
-			BrickTop = Brickobj->get_y(x,y);
+			BrickTop = Brickobj->get_y(x, y);
 			BrickBottom = BrickTop + BrickHeight;
 
+			//벽돌배열에 접근하기위한 인덱스
 			int index;
-			if (y== 0)
+			if (y == 0)
 			{
 				index = x + y;
 			}
@@ -113,7 +117,6 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 			}
 
 			// 공하고 벽돌하고 충돌 체크
-
 			// 상하가 부딪치는 경우
 			if (BallLeft == BrickLeft)
 			{
@@ -125,7 +128,7 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 					return true;
 				}
 				// 윗면 충돌
-				if (!ballobj->BallMoveY && (BallBottom <= BrickBottom) && ( BrickBottom- BallBottom  <= BrickHeight))
+				if (!ballobj->BallMoveY && (BallBottom <= BrickBottom) && (BrickBottom - BallBottom <= BrickHeight))
 				{
 					Brickobj->delete_bricks(index);
 					ballobj->BallMoveY = !ballobj->BallMoveY;
@@ -163,12 +166,12 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 				}
 			}
 
-				
+
 			// 좌우가 부딪치는 경우
 			if (BallTop == BrickTop && BallBottom == BrickBottom)
 			{
 				//왼쪽면 충돌
-				if (ballobj->BallMoveX && (BallRight <= BrickLeft) && (BrickLeft-BallRight  <= 0))
+				if (ballobj->BallMoveX && (BallRight <= BrickLeft) && (BrickLeft - BallRight <= 0))
 				{
 					Brickobj->delete_bricks(index);
 					ballobj->BallMoveX = !ballobj->BallMoveX;
@@ -183,7 +186,7 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 				}
 			}
 			//벽돌이 위에서 올때
-			else if (BallBottom<=BrickTop && (BrickTop-BallTop<=R))
+			else if (BallBottom <= BrickTop && (BrickTop - BallTop <= R))
 			{
 				if (ballobj->BallMoveX && (BallRight <= BrickLeft) && (BrickLeft - BallRight <= 0))
 				{
@@ -199,7 +202,7 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 				}
 			}
 			//벽돌이 아래에서 올때
-			else if (BallTop>=BrickBottom && BallBottom-BrickBottom<=R)
+			else if (BallTop >= BrickBottom && BallBottom - BrickBottom <= R)
 			{
 				if (ballobj->BallMoveX && (BallRight <= BrickLeft) && (BrickLeft - BallRight <= 0))
 				{
@@ -218,11 +221,11 @@ bool Ball::f_BALL_Brick(Ball* ballobj, Brick* Brickobj)
 	}
 	//충돌하지 않았을때
 	return false;
-	
+
 }
 
-	
-	
+
+
 
 
 
@@ -242,29 +245,29 @@ bool Ball::f_BALL_Bar(Ball* ballobj, Bar* Barobj)
 	}
 	int BallBottom = ballobj->BallY + R;
 	int BallLeft = ballobj->BallX;
-	int BallRight = ballobj->BallX + 2*R;
-	
+	int BallRight = ballobj->BallX + 2 * R;
+
 	int BarTop = Barobj->Get_Y();
-	int BarBottom= Barobj->Get_Y()+BarHeight;
+	int BarBottom = Barobj->Get_Y() + BarHeight;
 	int BarLeft = Barobj->Get_X();
-	int BarRight = Barobj->Get_X()+Barobj->Get_Size(); //+레벨에 따른 길이;
-	
+	int BarRight = Barobj->Get_X() + Barobj->Get_Size(); //+레벨에 따른 길이;
+
 	// 사각형 바와 원형 공의 충돌 처리
 	// 높이
-	if (BarTop >= BallBottom  && (BarTop-BallBottom<=0))
+	if (BarTop >= BallBottom && (BarTop - BallBottom <= 0))
 	{
 		// 좌우
-		if (BallLeft<=BarLeft && BallRight>=BarLeft)
+		if (BallLeft <= BarLeft && BallRight >= BarLeft)
 		{
 			ballobj->BallMoveY = !ballobj->BallMoveY;
 			return true;
 		}
-		else if (BallLeft>=BarLeft && BallRight<=BarRight)
+		else if (BallLeft >= BarLeft && BallRight <= BarRight)
 		{
 			ballobj->BallMoveY = !ballobj->BallMoveY;
 			return true;
 		}
-		else if (BallLeft<=BarRight && BallRight>=BarRight)
+		else if (BallLeft <= BarRight && BallRight >= BarRight)
 		{
 			ballobj->BallMoveY = !ballobj->BallMoveY;
 			return true;
@@ -286,12 +289,14 @@ bool Ball::f_BALL_Board(Ball* ballobj)
 	{
 		return false;
 	}
-		
-	int BallTop = ballobj->BallY;
-	int BallBottom = ballobj->BallY+R;
-	int BallLeft = ballobj->BallX;
-	int BallRight = ballobj->BallX + 2*R;
 
+	int BallTop = ballobj->BallY;
+	int BallBottom = ballobj->BallY + R;
+	int BallLeft = ballobj->BallX;
+	int BallRight = ballobj->BallX + 2 * R;
+
+
+	//설정한 게임창의 크기(경계)
 	int BoardLeft = 0;
 	int BoardRight = 50;
 	int BoardTop = 0;
@@ -308,8 +313,8 @@ bool Ball::f_BALL_Board(Ball* ballobj)
 	// 오른족
 	if (BallRight == BoardRight)
 	{
-	
-		ballobj->BallX = BoardRight - 2*R; // 위치 지정
+
+		ballobj->BallX = BoardRight - 2 * R; // 위치 지정
 		ballobj->BallMoveX = !ballobj->BallMoveX;
 		return true;
 	}
@@ -317,7 +322,7 @@ bool Ball::f_BALL_Board(Ball* ballobj)
 	// 위
 	if (BallTop == BoardTop)
 	{
-		ballobj->BallY = BoardTop ; // 위치 지정
+		ballobj->BallY = BoardTop; // 위치 지정
 		ballobj->BallMoveY = !ballobj->BallMoveY;
 		return true;
 	}
@@ -351,6 +356,7 @@ void Ball::f_MoveBall(Ball* ballobj)
 	else
 		ballobj->Ball_Direct = Right_down;
 
+
 	switch (ballobj->Ball_Direct)
 	{
 	case 0:
@@ -372,15 +378,18 @@ void Ball::f_MoveBall(Ball* ballobj)
 	}
 }
 
+
 //콘솔창에 Ball을 출력해주는 함수
 void Ball::Render()
 {
 	gotoxy(BallX, BallY);
 	cout << "◎";
 }
+
+//공의 실패위치를 판정해주는 함수
 bool Failed(const Ball* ballobj)
 {
-	if (ballobj->BallY>30)
+	if (ballobj->BallY > 30)
 		return true;
 	else
 		return false;
